1d14dea2ce0c42539f328abcc437fce7d5edac58
Anna Louise Chen <alchen@data.cs.purdue.edu>
2021-09-12 15:30:39 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -1,38 +1,38 @@
-#
+
 !/usr/bin/env bash
 
 # Changes to the ORIGIN directory
 cd ORIGIN
 
-# Task 1 
-mv Homework/MA165/HW2.txt HW1.txt
+# Task 1
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-rm Memes/Spongebob/money.jpeg
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-mv fafsa.jpg Memes/Spongebob
+mv Memes/'Star Wars'/fafsa.jpg Memes/SpongeBob
 
 # Task 4
-rm Homework/CS191
+rm -r  Homework/CS191
 
 # Task 5
 mkdir Programs/C++
 
 # Task 6
-mv Programs/App.cpp Programs/C++
+mv */Age.cpp Programs/C++
 
 # Task 7
-cp Programs/Java Homework/CS180 mv Project_1
+mkdir Homework/CS180/Project_1 cp Programs/Java/*.* Homework/CS180/Project_1_
 
 # Task 8
 mv Programs/*.c Programs/C
 
 # Task 9
-mv Programs/*.py Programs/Python3
+mv Programs/*.py Programs/'Python 3'
 
 # Task 10
-touch Homework/POL141 mv Essay.txt
+touch Homework/POL141/Essay.txt
 
 # Task 11
 ls ..
@@ -40,9 +40,6 @@ ls ..
 # Task 12
 : '
 Type your vim/nano customizations below:
-:set mouse=a
-:colorscheme onedark
-:set ttyfast
-
-
-'
+:set showmatch
+:set number
+:set cursorline
